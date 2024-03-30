6ed5c5f104ba7a6957f692f96ae21975bf3d1d1e
gaurav1954 <500090993@stu.upes.ac.in>
2024-03-30 10:07:21 +0530

    remoed unnecessary files

--- abc.js
+++
@@ -1,9 +0,0 @@
-const fun = async () => {
-    const apiUrl = `http://localhost:8000/recipes/like-status`; // API endpoint to get like status
-    const response = await fetch(apiUrl, {
-        method: 'GET',
-    });
-    const data = await response.json();
-    console.log(!data.one);
-}
-fun()

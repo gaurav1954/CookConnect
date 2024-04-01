28bd5b5c8fce51b9fd2e99026536c7ee7ce162c5
gaurav1954 <500090993@stu.upes.ac.in>
2024-04-01 16:24:30 +0530

    updated post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,7 +3,6 @@ import './Post.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as farHeart, faBookmark as farBookmark } from '@fortawesome/free-regular-svg-icons';
 import { faComment } from '@fortawesome/free-regular-svg-icons';
-import Like from './Like';
 function Post({ author = "gaurav", likes, description = "xys" }) {
     return (
         <div className='centered-container'>
@@ -12,18 +11,20 @@ function Post({ author = "gaurav", likes, description = "xys" }) {
                     <div className="profile-pic">
                         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt="Profile Icon" className="profile-icon" />
                         <span className="username">{author}</span>
-                        <div className="right-content">
-                            <FontAwesomeIcon icon={farBookmark} size="1x" className="bookmark-icon" />
-                        </div>
+
                     </div>
                 </div>
-                <div>
-                    <img src="https://plus.unsplash.com/premium_photo-1711477326347-721652a2a763?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='post-body' />
+                <img src="https://plus.unsplash.com/premium_photo-1711477326347-721652a2a763?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='post-image' />
+                <div className="likeAndSave">
+                    <div className="leftContent">
+
+                    </div>
+                    <div className="right-content">
+                        <FontAwesomeIcon icon={farBookmark} size="1x" className="bookmark-icon" />
+                    </div>
                 </div>
-                {/* like-comment-bookmark icon */}
-                <Like recipeId="65f5deae39d4d5ebd8dd729c" likes="3"></Like>
-                <div className="user-profile">
-                    <p>{description}</p>
+                <div className="post=description">
+                    {description}
                 </div>
             </div>
         </div>

291ab1762acdddbcd9fd23ebf7cffcac768b88a8
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2024-04-28 02:05:25 +0530

    New page

---
+++ frontend/src/components/PROFILE_INFO.jsx
@@ -0,0 +1,34 @@
+import REACT from 'react';
+import './PROFILE_INFO.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
+function PROFILE_INFO(){
+    return(
+        <div>
+            <div className='container'>
+            <div className="details">
+                <h3>Name: Kanishka</h3>
+                <h3>Age: 20</h3>
+                <h3>Location: Dehradun, Uttrakhand</h3>
+                <h3>Bio:</h3>
+                <h3>Favourite Cuisine: Indian</h3>
+                <h3>Cooking Experience: Noob</h3>
+                <h3>Allergies: Dry fruits</h3>
+                <h3>Instagram:</h3>
+            </div>
+            <div>
+            <img className="pic" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+            </div>
+            <br></br>
+            </div>
+            <hr className='division'/>
+            <div>
+                <h1 className='created'>Created</h1>
+                <h3 className='no_recipe_text'>No recipes published just yet!</h3>
+                <div><FontAwesomeIcon icon={faPlusCircle} className="plus-icon" /></div>
+            </div>
+        </div>
+    )
+}
+export default PROFILE_INFO
